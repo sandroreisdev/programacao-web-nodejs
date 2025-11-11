8a09d68a0dec74ebf911f5c1e750250e346d23f3
sandroreisdev <sandroreisdev@gmail.com>
2025-11-11 10:23:44 -0300

    nodejs-aula01-lib-express-rota-get

---
+++ app.js
@@ -0,0 +1,35 @@
+// const express = require('express')
+// const app = express()
+// app.use(express.json())
+// let port = 8081
+// app.listen(port, () => {
+//     console.log("Servidor rodando na porta 8081")
+// })
+
+// app.get('/', (req, res) => {
+//     res.send('Página index')
+// })
+
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Página index');
+});
+
+app.get('/sobre', (req, res) => {
+    res.send('Página sobre');
+});
+
+app.get('/contato', (req, res) => {
+    res.send('Página contato');
+});
+
+let port = 8081;
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
+
+

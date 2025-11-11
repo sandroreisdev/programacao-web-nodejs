c77e0738a81515f046799d48a7a05d4f79b66fac
sandroreisdev <sandroreisdev@gmail.com>
2025-11-11 11:53:57 -0300

    criando endpoint get e post

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-// const express = require('express')
-// const app = express()
-// app.use(express.json())
-// let port = 8081
-// app.listen(port, () => {
-//     console.log("Servidor rodando na porta 8081")
-// })
-
-// app.get('/', (req, res) => {
-//     res.send('Página index')
-// })
-
 const express = require('express');
 const app = express();
 
 app.use(express.json());
 
+let array = [
+    {nome: "Sandro", idade:52},
+    {nome: "Pedro", idade:48},
+    {nome: "Flávio", idade:46}
+]
 
+// array.push({nome:"Rosana", idade:45})
 
-app.get('/', (req, res) => {
-    res.send('Página index');
+app.post('/cadastro', (req,res) => {
+    const novoDado = req.body
+    array.push(novoDado)
+    res.send("usuário cadastrado com sucesso")
+})
+
+app.get('/', (req, res) => { // criando um end point
+    res.send(array);
 });
 
 app.get('/sobre', (req, res) => {
